e880f0069ec16e06eb0fb730e24ed0b8104c6147
EvilMuffinHa <47578209+EvilMuffinHa@users.noreply.github.com>
2020-02-09 17:24:13 -0500

    Create Tree.py

---
+++ Tree/Tree.py
@@ -0,0 +1,37 @@
+class Error(Exception):
+    pass
+
+class TreeError(Error):
+    pass
+
+
+class Tree:
+    def __init__(self):
+        self.base = ''
+        self.values = []
+    def __str__(self):
+        return '(' + str(self.base) + ' -> ' + str(self.values) + ')'
+    def __repr__(self):
+        return '(' + str(self.base) + ' -> ' + str(self.values) + ')'
+    def overwriteBase(self, base):
+        if isinstance(base, Tree):
+            raise TreeError('Cannot have a tree as a base')
+        else:
+            self.base = base
+    def appendChild(self, values):
+        self.values.append(values)
+    def appendChildren(self, *args):
+        if self.base == '':
+            raise TreeError('Cannot have a child without a base')
+        else:
+            for arg in args:
+                self.values.append(arg)
+    def deleteChild(self, location):
+        try:
+            x = self.values[location]
+            del self.values[location]
+            return x
+        except:
+            raise TreeError('Child does not exist')
+
+
