b83b3046ca5f8897cddfd7b0de70130c808f02bc
EvilMuffinHa <47578209+EvilMuffinHa@users.noreply.github.com>
2020-02-10 21:13:00 -0500

    Update Tree.py

--- Tree/Tree.py
+++ Tree/Tree.py
@@ -11,37 +11,37 @@ class BinTreeError(TreeError):
 class Tree:
     def __init__(self):
         self.base = ''
-        self.values = []
+        self.children = []
     def __str__(self):
-        return '(' + str(self.base) + ' -> ' + str(self.values) + ')'
+        return '(' + str(self.base) + ' -> ' + str(self.children) + ')'
     def __repr__(self):
-        return '(' + str(self.base) + ' -> ' + str(self.values) + ')'
+        return '(' + str(self.base) + ' -> ' + str(self.children) + ')'
     def overwriteBase(self, base):
         if isinstance(base, Tree):
             raise TreeError('Cannot have a tree as a base')
         else:
             self.base = base
-    def appendChild(self, values):
-        self.values.append(values)
+    def appendChild(self, children):
+        self.children.append(children)
     def appendChildren(self, *args):
         for arg in args:
-            self.values.append(arg)
+            self.children.append(arg)
     def deleteChild(self, location):
         try:
-            x = self.values[location]
-            del self.values[location]
+            x = self.children[location]
+            del self.children[location]
             return x
         except:
             raise TreeError('Child does not exist')
 
 #Binary trees
 class BinTree(Tree):
-    def appendChild(self, values):
-        if len(self.values) < 2:
-            self.values.append(values)
+    def appendChild(self, children):
+        if len(self.children) < 2:
+            self.children.append(children)
         else:
             raise BinTreeError('Binary Trees can only have two children. ')
-    def appendChildren(self, values):
-        raise BinTreeError('Binary Trees cannot append multiple values. ')
+    def appendChildren(self, children):
+        raise BinTreeError('Binary Trees cannot append multiple children. ')
 
 
